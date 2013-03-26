40038cdbcf163d7ff54b30b4409393833404800a
Jumi 3000 <jumi3000@Jumokes-MacBook-Pro.local>
2013-03-26 19:41:14 -0400

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '05134eb8c59200c882138f5f603e0a955447d001ee337a3d15c803c81a98e592b65ac28abd1fc2e65ece9cae73d044b9072563ceb74e5562030d135ff5f8c63b'
